322f87687bfa565d00bd9dcb3886e8ae6c3ef9a7
Pratik <prateekpatil9999@gmail.com>
2021-08-14 19:15:21 +0530

    woohooo project is completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
-
+import people from './data'
 function App() {
+  const [index,setIndex]=useState(1);
+  const {id,name,job,image,text}=people[index];
+ const  prevPerson=()=>{
+     const newIndex=index===0?people.length-1:index-1;
+       setIndex(newIndex);
+      return;
+   }
+  const nextPerson=()=>{
+    const newIndex=index===people.length-1?0:index+1;
+       setIndex(newIndex);
+      return;
+   }
+   const randomPerson=()=>{
+     const newIndex=Math.floor(Math.random()*people.length);
+     setIndex(newIndex);
+     return;
+   }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="title">Review</div>
+      
+      <div className="container">
+        <div className="img-container">
+          <img src={image} alt={id} className="person-img" />
+        </div>
+        <div className="review">
+          <h3 className="author">{name}</h3>
+          <h4 className="job">{job}</h4>
+          <p className="info">{text}</p>
+       
+        <div className="btn-container">
+          <button className="prev-btn" onClick={prevPerson}>{'<'}</button>
+          <button className="next-btn" onClick={nextPerson}>{'>'}</button>
+          </div>
+          <button className="random-btn" onClick={randomPerson}>Surprise me</button>
+        </div>
+      </div>
+
+    </main>
   );
 }
 
